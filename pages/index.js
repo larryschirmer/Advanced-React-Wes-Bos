cde0487f28bc1032bfec778c1ced614eebd19fe3
Larry Schirmer <larryschirmer@gmail.com>
2018-10-20 18:46:28 -0400

    add queried items to home and sell pages

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
 
+import Items from '../components/Items';
+
 const Home = props => {
   return (
     <div>
       <Link href="/sell">
-        <a>Hey!</a>
+        <Items />
       </Link>
     </div>
   );
